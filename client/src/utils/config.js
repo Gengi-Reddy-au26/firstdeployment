ea58af803b4deb53a2af0da8e554ce6e76179e71
Gangareddy N <gangareddyn17@gmail.com>
2023-03-02 09:17:10 +0530

    test

---
+++ client/src/utils/config.js
@@ -0,0 +1,20 @@
+export const BASE_URL = "http://localhost:4000/api/v1";
+
+
+// env: {
+//     ROOT: __dirname,
+
+    
+
+//     BASE_URL = "http://localhost:4000/api/v1";
+//     // API: "https://platovise.vercel.app", ENVIRONMENT:'prod',
+//     //API: "https://yellowcollartest.vercel.app", ENVIRONMENT: "prod",
+//     BASE_URL = 
+
+//     //API: "https://yellowcollarshakir.vercel.app", ENVIRONMENT:'prod',
+//     // API: "https://localhost:3000", ENVIRONMENT: "dev",
+//     DOMAIN_URL: "https://localhost:3000",
+
+//     GREETING: "Hello World ",
+
+// }
